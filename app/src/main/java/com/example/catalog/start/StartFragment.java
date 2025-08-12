ec5a1e22e8b1da4e415de8123eabb28efd8487a8
Radchenko Artem <ddruming@yandex.ru>
2025-08-13 00:10:13 +0300

    refactor: Удален лишний код

--- app/src/main/java/com/example/catalog/start/StartFragment.java
+++ app/src/main/java/com/example/catalog/start/StartFragment.java
@@ -1,4 +1,43 @@
 package com.example.catalog.start;
 
-public class StartFragment {
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.catalog.R;
+
+public class StartFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_start, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button loginButton = view.findViewById(R.id.button_login);
+        Button registerButton = view.findViewById(R.id.button_register);
+
+        loginButton.setOnClickListener(v -> {
+                    Log.d("StartFragment", "Нажата кнопка Авторизоваться");
+                }
+        );
+
+        registerButton.setOnClickListener(v -> {
+                    Log.d("StartFragment", "Нажата кнопка Зарегистрироваться");
+                }
+        );
+    }
 }
