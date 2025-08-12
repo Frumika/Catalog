1a55c27b454fa448a5745da9e8f8ad5655346a43
Radchenko Artem <ddruming@yandex.ru>
2025-08-12 21:14:04 +0300

    refactor: убраны лишние зависимости, используется новейшие версии библиотек

--- app/src/main/java/com/example/catalog/core/MainActivity.java
+++ app/src/main/java/com/example/catalog/core/MainActivity.java
@@ -1,17 +1,10 @@
 package com.example.catalog.core;
 
 import android.os.Bundle;
-import android.util.Log;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.navigation.NavController;
-import androidx.navigation.fragment.NavHostFragment;
+
 
 import com.example.catalog.R;
 
