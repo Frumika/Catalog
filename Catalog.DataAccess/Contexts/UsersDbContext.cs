27a07b4671d8984c13eba40e7baaf8a530142cb5
Radchenko Artem <ddruming@yandex.ru>
2025-09-02 19:15:11 +0300

    feat: Написал модель User и UserDbContext для доступа к базе данных

---
+++ Catalog.DataAccess/Contexts/UsersDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Catalog.DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.DataAccess.Contexts;
+
+public class UsersDbContext : DbContext
+{
+    private const int StringLength = 255;
+
+    public DbSet<User> Users { get; set; }
+
+    
+    public UsersDbContext(DbContextOptions<UsersDbContext> options) : base(options)
+    {
+    }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.ToTable("Users");
+            entity.HasKey(user => user.Id);
+            entity.Property(user => user.Id).ValueGeneratedOnAdd();
+
+            entity.Property(user => user.Login).HasMaxLength(StringLength);
+            entity.HasIndex(user => user.Login).IsUnique();
+
+            entity.Property(user => user.Email).HasMaxLength(StringLength);
+            entity.HasIndex(user => user.Email).IsUnique();
+
+            entity.Property(user => user.Password).HasMaxLength(StringLength);
+        });
+    }
+}
---
+++ Catalog.DataAccess/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Catalog.DataAccess.Models;
+
+public class User
+{
+    public long Id { get; set; }
+    public string Email { get; set; } = string.Empty;
+    public string Login { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
