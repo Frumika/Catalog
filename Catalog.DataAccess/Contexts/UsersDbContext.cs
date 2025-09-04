5007c797bfc913892d0dd4c644830bc9075f70de
Radchenko Artem <ddruming@yandex.ru>
2025-09-04 13:35:15 +0300

    feat: Добавлен логгинг запросов к БД в UsersDbContext
    Добавлена настройка логирования запросов к базе данных в UsersDbContext и обновлен уровень логгирования для Microsoft.EntityFrameworkCore в appsettings.json

--- Catalog.DataAccess/Contexts/UsersDbContext.cs
+++ Catalog.DataAccess/Contexts/UsersDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Catalog.Domain.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace Catalog.DataAccess.Contexts;
 
@@ -9,11 +10,16 @@ public class UsersDbContext : DbContext
 
     public DbSet<User> Users { get; set; }
 
-    
+
     public UsersDbContext(DbContextOptions<UsersDbContext> options) : base(options)
     {
     }
 
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.LogTo(_ => { }, LogLevel.Warning);
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
