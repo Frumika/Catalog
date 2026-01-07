80d906d1bd862c3a695f9c93e3827fe67ddeb081
Radchenko Artem <ddruming@yandex.ru>
2026-01-07 20:55:47 +0300

    feat: созданы OrderResponse и OrderStatusCode

---
+++ Backend/Backend.Application/DTO/Responses/OrderResponse.cs
@@ -0,0 +1,17 @@
+﻿using Backend.Application.StatusCodes;
+
+
+namespace Backend.Application.DTO.Responses;
+
+public class OrderResponse : BaseResponse<OrderStatusCode, OrderResponse>
+{
+    public new static OrderResponse Success<TData>(TData? data = null, string? message = null) where TData : class
+    {
+        return CreateSuccess(OrderStatusCode.Success, data, message);
+    }
+
+    public new static OrderResponse Success(string? message = null)
+    {
+        return CreateSuccess(OrderStatusCode.Success, message);
+    }
+}
