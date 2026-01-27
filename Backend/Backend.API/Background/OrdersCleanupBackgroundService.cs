49488d6ca2ad0cfa42f823cc779d6f1d6a71150b
Radchenko Artem <ddruming@yandex.ru>
2026-01-28 00:01:41 +0300

    styly: убрал лишние комментарии

--- Backend/Backend.API/Background/OrdersCleanupBackgroundService.cs
+++ Backend/Backend.API/Background/OrdersCleanupBackgroundService.cs
@@ -1,17 +1,18 @@
 ﻿using Backend.Application.Services;
 
+
 namespace Backend.API.Background;
 
 public class OrdersCleanupBackgroundService : BackgroundService
 {
     private readonly IServiceScopeFactory _scopeFactory;
     private readonly TimeSpan _interval = TimeSpan.FromMinutes(1);
-    
+
     public OrdersCleanupBackgroundService(IServiceScopeFactory scopeFactory)
     {
         _scopeFactory = scopeFactory;
     }
-    
+
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         while (!stoppingToken.IsCancellationRequested)
@@ -22,20 +23,15 @@ public class OrdersCleanupBackgroundService : BackgroundService
                 var cleanupService = scope.ServiceProvider.GetRequiredService<OrdersCleanupService>();
 
                 await cleanupService.CleanupExpiredOrdersAsync(stoppingToken);
-            }
-            catch (Exception ex)
-            {
-                // логирование ошибок, чтобы не убить цикл
-                Console.WriteLine($"Error in ReservationCleanupHostedService: {ex}");
-            }
-            
-            try
-            {
                 await Task.Delay(_interval, stoppingToken);
             }
             catch (TaskCanceledException)
             {
                 break;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error in ReservationCleanupHostedService: {ex}");
             }
         }
     }
