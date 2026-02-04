9fbaa2d37aa0bc3d857101e1a5ffe0201b88c38c
Radchenko Artem <ddruming@yandex.ru>
2026-02-04 20:14:30 +0300

    refactor: убрал неиспользующееся подключение к Redis

--- Backend/Backend.DataAccess/Redis/RedisDbProvider.cs
+++ Backend/Backend.DataAccess/Redis/RedisDbProvider.cs
@@ -6,13 +6,11 @@ namespace Backend.DataAccess.Redis;
 public class RedisDbProvider
 {
     private const int UserSessionsDbIndex = 0;
-    private const int CartsStatesDbIndex = 1;
-    private const int OrdersStatesDbIndex = 2;
+    private const int OrdersStatesDbIndex = 1;
     
     private readonly IConnectionMultiplexer _dbConnection;
     
     public IDatabase UserSessions => _dbConnection.GetDatabase(UserSessionsDbIndex);
-    public IDatabase CartStates => _dbConnection.GetDatabase(CartsStatesDbIndex);
     public IDatabase OrderStates => _dbConnection.GetDatabase(OrdersStatesDbIndex);
 
     public RedisDbProvider(IConnectionMultiplexer dbConnection)
