0b11225973ecd781f3e0475b1441d965215a57ab
Radchenko Artem <ddruming@yandex.ru>
2026-06-14 20:01:54 +0300

    feat(frontend): написал Input

---
+++ catalog-frontend/src/shared/ui/input/Input.tsx
@@ -0,0 +1,30 @@
+import type {InputProps} from "@/shared/ui/input/Input.types.ts";
+import styles from "./Input.module.css"
+import type {ChangeEvent} from "react";
+
+export const Input = (
+    {
+        value,
+        onChange,
+        className,
+        ...props
+    }: InputProps) => {
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        onChange?.(event.target.value)
+    }
+
+    const inputClasses = [
+        styles.input,
+        className,
+    ].filter(Boolean).join(' ');
+
+    return (
+        <input
+            className={inputClasses}
+            value={value}
+            onChange={handleChange}
+            {...props}
+        />
+    );
+}
