a6f863b54d852c0439dc732cdc3b50b8bc340bca
Radchenko Artem <ddruming@yandex.ru>
2026-07-07 17:17:25 +0300

    refactor(backend): восстановил PickupPointService

--- Backend/Backend.Application/Services/PickupPoints/PickupPointService.cs
+++ Backend/Backend.Application/Services/PickupPoints/PickupPointService.cs
@@ -1,20 +1,21 @@
 ﻿using Backend.Application.Common;
 using Backend.Application.Common.Base;
 using Backend.Application.Common.Statuses;
+using Backend.Application.DataAccess.Contexts;
 using Backend.Application.Services.PickupPoints.Dtos;
 using Backend.Application.Services.PickupPoints.Requests;
 using Backend.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace Backend.Application.Services.PickupPoints;
 
 public class PickupPointService
 {
-    private readonly IBaseRepository _baseRepository;
-    private readonly IPickupPointRepository _dbContext;
+    private readonly MainDbContext _dbContext;
 
-    public PickupPointService(IBaseRepository baseRepository, IPickupPointRepository dbContext)
+    public PickupPointService(MainDbContext dbContext)
     {
-        _baseRepository = baseRepository;
         _dbContext = dbContext;
     }
 
@@ -22,11 +23,37 @@ public class PickupPointService
     {
         try
         {
-            int? userId = await _baseRepository.GetUserIdAsync(sessionId);
+            int? userId = await _dbContext.UserSessions
+                .AsNoTracking()
+                .Where(ui => ui.UId == sessionId)
+                .Select(ui => (int?)ui.UserId)
+                .FirstOrDefaultAsync();
             if (userId is null)
                 return Response.Fail(new UserNotFound(), "The user wasn't found");
 
-            List<PickupPointDto> points = await _dbContext.GetPickupPoints((int)userId);
+            var rawPoints = await _dbContext.UserPickupPoints
+                .AsNoTracking()
+                .Where(upp => upp.UserId == userId)
+                .Select(upp => new
+                {
+                    upp.SelectedAt,
+                    upp.PickupPoint
+                })
+                .ToListAsync();
+
+            List<PickupPointDto> points = rawPoints
+                .Select(x =>
+                    new PickupPointDto
+                    {
+                        Id = x.PickupPoint.Id,
+                        Address = MakePickupPointAddress(x.PickupPoint),
+                        ShelfLifetime = x.PickupPoint.ShelfLifetime,
+                        SelectedAt = x.SelectedAt
+                    }
+                )
+                .OrderByDescending(x => x.SelectedAt)
+                .ToList();
+
             return Response.Success(points);
         }
         catch (Exception)
@@ -43,11 +70,21 @@ public class PickupPointService
 
         try
         {
-            int? userId = await _baseRepository.GetUserIdAsync(request.UserSessionId);
+            int? userId = await _dbContext.UserSessions
+                .AsNoTracking()
+                .Where(ui => ui.UId == request.UserSessionId)
+                .Select(ui => (int?)ui.UserId)
+                .FirstOrDefaultAsync();
             if (userId is null)
                 return Response.Fail(new UserNotFound(), "The user wasn't found");
 
-            UserPickupPoint? userPoint = await _dbContext.GetUserPickupPoint((int)userId, request.PickupPointId);
+            UserPickupPoint? userPoint = await _dbContext.UserPickupPoints
+                .Where(upp =>
+                    upp.UserId == userId &&
+                    upp.PickupPointId == request.PickupPointId
+                )
+                .Include(upp => upp.PickupPoint)
+                .FirstOrDefaultAsync();
 
             if (userPoint is null)
                 return Response.Fail(new PickupPointNotFound(), "The pickup point was not found");
@@ -55,13 +92,13 @@ public class PickupPointService
             DateTime currentTime = DateTime.UtcNow;
 
             userPoint.SelectedAt = currentTime;
-            await _baseRepository.CommitAsync();
+            await _dbContext.SaveChangesAsync();
 
             return Response.Success(
                 new PickupPointDto
                 {
                     Id = userPoint.PickupPoint.Id,
-                    Address = PickupPointAddressFormatter.Format(userPoint.PickupPoint),
+                    Address = MakePickupPointAddress(userPoint.PickupPoint),
                     ShelfLifetime = userPoint.PickupPoint.ShelfLifetime,
                     SelectedAt = currentTime
                 });
@@ -80,11 +117,17 @@ public class PickupPointService
 
         try
         {
-            int? userId = await _baseRepository.GetUserIdAsync(request.UserSessionId);
+            int? userId = await _dbContext.UserSessions
+                .AsNoTracking()
+                .Where(ui => ui.UId == request.UserSessionId)
+                .Select(ui => (int?)ui.UserId)
+                .FirstOrDefaultAsync();
             if (userId is null)
                 return Response.Fail(new UserNotFound(), "The user wasn't found");
 
-            await _dbContext.DeleteUserPickupPoint((int)userId, request.PickupPointId);
+            await _dbContext.UserPickupPoints
+                .Where(upp => upp.UserId == userId && upp.PickupPointId == request.PickupPointId)
+                .ExecuteDeleteAsync();
 
             return Response.Success("The record was successfully deleted.");
         }
@@ -92,5 +135,29 @@ public class PickupPointService
         {
             return Response.Fail(new UnknownError(), "Internal server error");
         }
+    }
+
+    private string MakePickupPointAddress(PickupPoint point)
+    {
+        var streetAbbr = point.StreetType switch
+        {
+            StreetType.Street => "ул.",
+            StreetType.Avenue => "пр.",
+            StreetType.Lane => "пер.",
+            StreetType.Boulevard => "б-р",
+            StreetType.Highway => "ш.",
+            StreetType.Square => "пл.",
+            StreetType.Embankment => "наб.",
+            StreetType.Passage => "проезд",
+            StreetType.Alley => "ал.",
+            _ => point.StreetType.ToString()
+        };
+
+        var address = $"г. {point.City}, {streetAbbr} {point.StreetName}, д. {point.House}";
+
+        if (!string.IsNullOrEmpty(point.Building))
+            address += $", корп. {point.Building}";
+
+        return address;
     }
 }
