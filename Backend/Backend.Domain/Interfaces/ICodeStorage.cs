1e0f69088ebc94dc2d45d96a16716fd5c1f68d5b
Radchenko Artem <ddruming@yandex.ru>
2026-07-07 20:25:26 +0300

    feat(backend): наметил CodeStorage

---
+++ Backend/Backend.Domain/Interfaces/ICodeStorage.cs
@@ -0,0 +1,8 @@
+﻿namespace Backend.Domain.Interfaces;
+
+public interface ICodeStorage
+{
+    Task SaveCodeAsync(string email, string code, TimeSpan expiration);
+    Task<string?> GetCodeAsync(string email);
+    Task RemoveCodeAsync(string email);
+}
---
+++ Backend/Backend.Infrastructure/Services/Storage/RedisCodeStorage.cs
@@ -0,0 +1,21 @@
+﻿using Backend.Domain.Interfaces;
+
+namespace Backend.Infrastructure.Services.Storage;
+
+public class RedisCodeStorage : ICodeStorage
+{
+    public Task SaveCodeAsync(string email, string code, TimeSpan expiration)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<string?> GetCodeAsync(string email)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task RemoveCodeAsync(string email)
+    {
+        throw new NotImplementedException();
+    }
+}
