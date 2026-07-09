1d196827de78798e357d2a07c66d18a335ca06f8
Radchenko Artem <ddruming@yandex.ru>
2026-07-09 22:17:12 +0300

    bugfix(frontend): починил загрузку корзины

--- catalog-frontend/src/app/routes/AppRoutes.tsx
+++ catalog-frontend/src/app/routes/AppRoutes.tsx
@@ -1,15 +1,22 @@
 import {Route, Routes} from 'react-router-dom';
 import {HomePage} from "@/pages/home";
 import {CartPage} from "@/pages/cart";
-import { useCartStore } from '@/entities/cart';
-import { useEffect } from 'react';
+import {useCartStore} from '@/entities/cart';
+import {useEffect} from 'react';
+import {useIsAuthenticated} from "@/entities/session";
 
 export const AppRoutes = () => {
+    const isAuthenticated = useIsAuthenticated();
     const fetchCart = useCartStore(state => state.fetchCart);
 
     useEffect(() => {
-        fetchCart(); // Загружаем корзину один раз при старте приложения
-    }, [fetchCart]);
+        if (!isAuthenticated) {
+            return;
+        }
+
+        void fetchCart();
+    }, [isAuthenticated]);
+
     return (
         <Routes>
             <Route path="/" element={<HomePage/>}/>
